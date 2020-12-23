1bf8dad69fec047963591ff792dc0acbf7f691e2
chaitrarai00 <chaitrarai00@gmail.com>
2020-12-23 20:43:15 +0530

    singleton

--- src/creational/DesignPatternDriver.java
+++ src/creational/DesignPatternDriver.java
@@ -6,6 +6,9 @@ public class DesignPatternDriver {
 		Singleton s1=Singleton.getinstance();
 		Singleton s2=Singleton.getinstance();
 		System.out.println(s1+" "+s2+" "+s1.equals(s2));
+		
+		//one can break singleton using serialization/reflection
+		//junit uses reflection to break singleton
 	}
 
 }
--- src/creational/Singleton.java
+++ src/creational/Singleton.java
@@ -1,11 +1,31 @@
 package creational;
 
 public class Singleton {
-	private static Singleton instance_ofsingleton=new Singleton();
+	//not a good practise because unnecessary memory is accomodated
+	//private static Singleton instance_ofsingleton=new Singleton();
+	//instead use lazy intialization
+	private static Singleton instance_ofsingleton_lazy=null;
+
 	private Singleton() {
 		
 	}
+	/*
+	 * use synchronized to provide threadlock and avoid issues 
+	 * when multiple thread try to create singleton objects
+	 * using synchronized method will slow down the process 
+	 * since lock is added to the whole method
+	 * so instead use synchronized block and 
+	 * minimize the area synchronization has to be done in
+	 */
 	public static Singleton getinstance() {
-		return instance_ofsingleton;
+		if(instance_ofsingleton_lazy==null)
+			synchronized (Singleton.class) {
+				if(instance_ofsingleton_lazy==null) 
+				{
+				instance_ofsingleton_lazy= new Singleton();
+				}
+			}
+			
+		return instance_ofsingleton_lazy;
 	}
 }

b713dcc66c1ff89e36dfe227de61cf3eb13146ea
chaitrarai00 <chaitrarai00@gmail.com>
2021-01-12 13:30:24 +0530

    merge

--- src/creational/DesignPatterDriver_Abstract.java
+++ src/creational/DesignPatterDriver_Abstract.java
@@ -4,6 +4,8 @@ public class DesignPatterDriver_Abstract {
 /*
  * Its done by adding layers on the factory pattern
  * its mentioned or said to be Factory of Factories
+ * u get the FactoryInstance from the factopry and
+ *  then the respective device  instancefrom underlying abstract method
  */
 	public static void main(String[] args) {
 		Device device=FactoryGenerator.getFactory(FactoryType.MOBILEFACTORY).getdevice(DeviceType.ONEPLUS);
